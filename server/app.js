c12c868da470fbbab6c65b3f7a10bdcbc391710c
Phillip Alexander <phillip.alexander@galvanize.com>
2021-03-29 12:30:38 +0000

    (init)

---
+++ server/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var db = require('./db');
+
+// Middleware
+var morgan = require('morgan');
+
+// Router
+var router = require('./routes.js');
+
+var app = express();
+module.exports.app = app;
+
+// Set what we are listening on.
+app.set('port', 3000);
+
+// Logging and parsing
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Set up our routes
+app.use('/classes', router);
+
+// Serve the client files
+app.use(express.static(__dirname + '/../client'));
+
+// If we are being run directly, run the server.
+if (!module.parent) {
+  app.listen(app.get('port'));
+  console.log('Listening on', app.get('port'));
+}
+
