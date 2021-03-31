3a8ab22e92b8cb2c7bcadd1f3a422ce4d532dd07
Will Trinh <williamltrinh@gmail.com>
2021-03-31 16:50:03 -0700

    Implemented controllers

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,6 +1,25 @@
 var models = require('../models');
 
 module.exports = {
-  get: function (req, res) {}, // a function which handles a get request for all messages
-  post: function (req, res) {} // a function which handles posting a message to the database
+  get: function (req, res) {
+    res.statusCode = 200;
+    models.messages.getAll((err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.end(JSON.stringify(data));
+      }
+    });
+  }, // a function which handles a get request for all messages
+  post: function (req, res) {
+    res.statusCode = 201;
+    // create (messages, createdAt, roomname)
+    models.messages.create(req.body.messages, null, req.body.roomname, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.end();
+      }
+    });
+  } // a function which handles posting a message to the database
 };
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,25 @@
 var models = require('../models');
 
 module.exports = {
-  get: function (req, res) {},
-  post: function (req, res) {}
+  get: function (req, res) {
+    // statusCode
+    res.statusCode = 200;
+    models.users.getAll((err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.end(JSON.stringify(data));
+      }
+    });
+  },
+  post: function (req, res) {
+    res.statusCode = 201;
+    models.users.create(req.body.username, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.end();
+      }
+    });
+  }
 };
