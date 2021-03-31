6807c223504370092011a0e706c33f9e0a619fb5
Will Trinh <williamltrinh@gmail.com>
2021-03-31 15:17:35 -0700

    Implemented schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,27 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE rooms (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  roomname VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  message VARCHAR(150) NOT NULL,
+  user_id INT NOT NULL,
+  created_at DATE NOT NULL,
+  room_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
