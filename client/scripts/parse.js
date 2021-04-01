c2b3a80a19717eaf75fd75e2c5720cc256823056
Will Trinh <williamltrinh@gmail.com>
2021-03-31 19:51:49 -0700

    Added client codes

---
+++ client/scripts/parse.js
@@ -0,0 +1,32 @@
+var Parse = {
+
+  server: `https://app-hrsei-api.herokuapp.com/api/chatterbox/messages/${window.CAMPUS}`,
+
+  create: function(message, successCB, errorCB = null) {
+
+    $.ajax({
+      url: Parse.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: successCB,
+      error: errorCB || function (error) {
+        console.error('chatterbox: Failed to create message', error);
+      }
+    });
+  },
+
+  readAll: function(successCB, errorCB = null) {
+    $.ajax({
+      url: Parse.server,
+      type: 'GET',
+      data: { order: '-createdAt' },
+      contentType: 'application/json',
+      success: successCB,
+      error: errorCB || function(error) {
+        console.error('chatterbox: Failed to fetch messages', error);
+      }
+    });
+  }
+
+};
